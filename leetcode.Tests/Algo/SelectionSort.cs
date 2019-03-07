b758e394697e45914e248fa5b3d08b3282a5c924
dmitry <dmitry@fastzone.ru>
2019-03-07 16:54:15 +0300

    update SelectionSort

--- leetcode.Tests/Algo/SelectionSort.cs
+++ leetcode.Tests/Algo/SelectionSort.cs
@@ -10,8 +10,9 @@ namespace leetcode.Tests.Algo
     {
         [Theory]
         [InlineData(new[] { 5, 3, 6, 2, 10 }, new[] { 2, 3, 5, 6, 10 })]
-        //[InlineData(new[] { 5, 3, 6, 2, 10 }, new[] { 10, 6, 5, 3, 2})]
-        public void Test(int[] input, int[] expected)
+        [InlineData(new[] { 5, 3, 6, 2, 10, 1 }, new[] { 1, 2, 3, 5, 6, 10 })]
+        [InlineData(new[] { 5, 3, 6, 4, 2, 10, 1, 9 }, new[] { 1, 2, 3, 4, 5, 6, 9, 10 })]
+        public void SelectionSortTest(int[] input, int[] expected)
         {
             var s = new Solution();
             var res = s.Sort(input);
@@ -23,13 +24,12 @@ namespace leetcode.Tests.Algo
             public int[] Sort(int[] arr)
             {
                 if (arr.Length == 0) return arr;
-
                 
-                for (int i = 0; i < arr.Length; i++)
+                for (int i = 0; i < arr.Length - 1; i++)
                 {
                     int min = arr[i];
                     int minIdx = i;
-                    for (int j = i; j < arr.Length; j++)
+                    for (int j = i + 1; j < arr.Length; j++)
                     {
                         if (arr[j] < min)
                         {
