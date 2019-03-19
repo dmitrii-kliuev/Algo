afd7d17a297f8c151fd2ca48520a9d730d550d7b
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-03-19 15:02:31 +0300

    fibonacci fix

--- leetcode.Tests/Algo/DynamicProgramming/Fibonachi.cs
+++ leetcode.Tests/Algo/DynamicProgramming/Fibonachi.cs
@@ -10,26 +10,27 @@ namespace leetcode.Tests.Algo
         [Theory]
         [InlineData(0, 0)]
         [InlineData(1, 1)]
-        [InlineData(2, 2)]
-        [InlineData(3, 3)]
-        [InlineData(4, 5)]
-        [InlineData(5, 8)]
-        [InlineData(6, 13)]
-        [InlineData(7, 21)]
-        [InlineData(8, 34)]
-        [InlineData(9, 55)]
-        [InlineData(10, 89)]
-        [InlineData(11, 144)]
-        [InlineData(12, 233)]
-        [InlineData(13, 377)]
-        [InlineData(14, 610)]
-        [InlineData(15, 987)]
-        [InlineData(16, 1597)]
-        [InlineData(17, 2584)]
-        [InlineData(18, 4181)]
-        [InlineData(19, 6765)]
-        [InlineData(20, 10946)]
-        [InlineData(21, 17711)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(8, 21)]
+        [InlineData(9, 34)]
+        [InlineData(10, 55)]
+        [InlineData(11, 89)]
+        [InlineData(12, 144)]
+        [InlineData(13, 233)]
+        [InlineData(14, 377)]
+        [InlineData(15, 610)]
+        [InlineData(16, 987)]
+        [InlineData(17, 1597)]
+        [InlineData(18, 2584)]
+        [InlineData(19, 4181)]
+        [InlineData(20, 6765)]
+        [InlineData(21, 10946)]
+        [InlineData(22, 17711)]
         public void Test(int num, int expected)
         {
             var s = new Solution();
@@ -43,17 +44,17 @@ namespace leetcode.Tests.Algo
             {
                 if (n < 0) throw new Exception("Only positive allowed");
                 if (n == 0) return 0;
-                if (n == 1) return 1;
+                if (n == 1 || n == 2) return 1;
 
-                var f = new int[n + 1];
+                var f = new int[n];
                 f[0] = 1;
                 f[1] = 1;
-                for (int i = 2; i <= n; i++)
+                for (int i = 2; i < n; i++)
                 {
                     f[i] = f[i - 2] + f[i - 1];
                 }
 
-                return f[n];
+                return f[n - 1];
             }
         }
     }
