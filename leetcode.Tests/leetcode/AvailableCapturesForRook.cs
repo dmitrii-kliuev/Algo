997c6dbfc1456e39c35d0121278bcb0c97ac1123
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-03-22 11:15:41 +0300

    + AvailableCapturesForRook solution

--- leetcode.Tests/leetcode/AvailableCapturesForRook.cs
+++ leetcode.Tests/leetcode/AvailableCapturesForRook.cs
@@ -54,9 +54,134 @@ namespace leetcode.Tests.leetcode
 
          */
         [Fact]
-        public void Test()
+        public void Test0()
         {
+            var board = new char[][]
+                {
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','p','.','.','.','.'},
+                    new char []{'.','.','.','R','.','.','.','p'},
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','p','.','.','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'}
+                };
 
+
+            var s = new Solution();
+            var actual = s.NumRookCaptures(board);
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var board = new char[][]
+                {
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','p','p','p','p','p','.','.'},
+                    new char []{'.','p','p','B','p','p','.','.'},
+                    new char []{'.','p','B','R','B','p','.','.'},
+                    new char []{'.','p','p','B','p','p','.','.'},
+                    new char []{'.','p','p','p','p','p','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'}
+                };
+
+
+            var s = new Solution();
+            var actual = s.NumRookCaptures(board);
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var board = new char[][]
+                {
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','p','.','.','.','.'},
+                    new char []{'.','.','.','p','.','.','.','.'},
+                    new char []{'p','p','.','R','.','p','B','.'},
+                    new char []{'.','.','.','.','.','.','.','.'},
+                    new char []{'.','.','.','B','.','.','.','.'},
+                    new char []{'.','.','.','p','.','.','.','.'},
+                    new char []{'.','.','.','.','.','.','.','.'}
+                };
+
+
+            var s = new Solution();
+            var actual = s.NumRookCaptures(board);
+            Assert.Equal(3, actual);
+        }
+
+        public class Solution
+        {
+            public int NumRookCaptures(char[][] board)
+            {
+                var res = 0;
+                var rRow = 0;
+                var rColumn = 0;
+
+                var rIsFound = false;
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (board[i][j] == 'R')
+                        {
+                            rRow = i;
+                            rColumn = j;
+                            rIsFound = true;
+                            break;
+                        }
+                    }
+
+                    if (rIsFound) break;
+                }
+
+                // up
+                for (int i = rRow - 1; i >= 0; i--)
+                {
+                    if (board[i][rColumn] == 'p')
+                    {
+                        res++;
+                        break;
+                    }
+                    else if (board[i][rColumn] == 'B')
+                        break;
+                }
+
+                // down
+                for (int i = rRow + 1; i < 8; i++)
+                {
+                    if (board[i][rColumn] == 'p')
+                    { res++; break; }
+                    else if (board[i][rColumn] == 'B')
+                        break;
+                }
+
+                // left
+                for (int j = rColumn + 1; j >= 0; j--)
+                {
+                    if (board[rRow][j] == 'p')
+                    { res++; break; }
+                    else if (board[rRow][j] == 'B')
+                        break;
+                }
+
+                // right
+                for (int j = rColumn - 1; j < 8; j++)
+                {
+                    if (board[rRow][j] == 'p')
+                    { res++; break; }
+                    else if (board[rRow][j] == 'B')
+                        break;
+                }
+
+                return res;
+            }
         }
     }
 }
