cd72cca31a2118e68914017ccef606cd8bc18a4a
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-03-27 18:05:53 +0300

    addition information about knapsack

--- leetcode.Tests/Algo/DynamicProgramming/Knapsack.cs
+++ leetcode.Tests/Algo/DynamicProgramming/Knapsack.cs
@@ -11,6 +11,8 @@ namespace leetcode.Tests.Algo.DynamicProgramming
         public void Test()
         {
             var res = GFG.Start();
+
+            var t = Knapsack1.Start();
         }
 
 
@@ -26,9 +28,9 @@ namespace leetcode.Tests.Algo.DynamicProgramming
                 //int[] wt = new int[] { 10, 20, 30 };        // weight. вес каждого
                 //int W = 50;                                 // максимальный вес рюкзака
                 //int n = val.Length;
-                                    //  гитара, магнитофон, ноутбук
-                int[] val = new int[] { 1500,   3000,       2000 };     // value. цена
-                int[] wt = new int[] {  1,      4,          3 };        // weight. вес
+                //  гитара, магнитофон, ноутбук
+                int[] val = new int[] { 1500, 3000, 2000 };     // value. цена
+                int[] wt = new int[] { 1, 4, 3 };        // weight. вес
                 int W = 4;                                              // максимальный вес рюкзака
                 int n = val.Length;
 
@@ -64,10 +66,129 @@ namespace leetcode.Tests.Algo.DynamicProgramming
                 // Return the maximum of two cases:  
                 // (1) nth item included  
                 // (2) not included 
-                else return max(val[n - 1] +
-                    knapSack(W - wt[n - 1], wt, val, n - 1),
+                else return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1),
                            knapSack(W, wt, val, n - 1));
             }
         }
+    }
+
+    // good video https://www.dyclassroom.com/dynamic-programming/0-1-knapsack-problem
+
+    class Knapsack1
+    {
+        // https://www.hackerearth.com/ru/practice/notes/the-knapsack-problem/
+        public static int Start()
+        {
+
+            var val = new[] { 10, 40, 30, 50 };
+
+            var wt = new[] { 5, 4, 6, 3 };
+
+            int W = 10;
+
+            var res = knapsack(val, wt, W);
+
+            return res;
+        }
+
+
+
+        public static int knapsack(int[] val, int[] wt, int W)
+        {
+
+            //Get the total number of items.
+
+            //Could be wt.length or val.length. Doesn't matter
+
+            int N = wt.Length;
+
+
+            //Create a matrix.
+
+            //Items are in rows and weight at in columns +1 on each side
+
+            int[,] V = new int[N + 1, W + 1];
+
+            //What if the knapsack's capacity is 0 - Set
+
+            //all columns at row 0 to be 0
+
+            for (int col = 0; col <= W; col++)
+            {
+
+                V[0, col] = 0;
+
+            }
+
+
+
+            //What if there are no items at home.
+
+            //Fill the first row with 0
+            for (int row = 0; row <= N; row++)
+            {
+
+                V[row, 0] = 0;
+            }
+
+            for (int item = 1; item <= N; item++)
+            {
+
+
+                //Let's fill the values row by row
+
+                for (int weight = 1; weight <= W; weight++)
+                {
+
+                    //Is the current items weight less
+
+                    //than or equal to running weight
+
+                    if (wt[item - 1] <= weight)
+                    {
+
+                        //Given a weight, check if the value of the current
+
+                        //item + value of the item that we could afford
+
+                        //with the remaining weight is greater than the value
+
+                        //without the current item itself
+
+                        V[item, weight] = Math.Max(val[item - 1] + V[item - 1, weight - wt[item - 1]], V[item - 1, weight]);
+                    }
+
+                    else
+                    {
+
+                        //If the current item's weight is more than the
+
+                        //running weight, just carry forward the value
+
+                        //without the current item
+
+                        V[item, weight] = V[item - 1, weight];
+                    }
+                }
+
+            }
+
+            //Printing the matrix
+
+            //foreach (var rows in V)
+            //{
+
+            //    foreach (int col in rows)
+            //    {
+
+            //        System.out.format("%5d", col);
+            //    }
+            //    System.out.println();
+            //}
+
+            return V[N, W];
+
+        }
+
     }
 }
