5cbeaada3abe7a2c3d8f519ef993db21935baf74
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-09-15 15:20:30 +0300

    + Breadth-First Search(BFS) + Depth-First Search(DFS) recursive and non-recursive

---
+++ leetcode.Tests/Algo/Graphs/Graph_BFS_DFS.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+
+namespace leetcode.Tests.Algo.Graphs
+{
+    public class Graph_BFS_DFS
+    {
+        [Fact]
+        public void Test()
+        {
+            double[,] arr = new double[,]
+            {
+                {0, 1, 1, 0},
+                {0, 0, 1, 1},
+                {1, 0, 0, 1},
+                {0, 1, 0, 0}
+            };
+
+            Graph g = new Graph(arr);
+
+            g.BFS(1);
+
+            //g.DFS(1);
+        }
+
+        [Fact]
+        public void Graph_DFS_Non_recursive()
+        {
+            double[,] arr = new double[,]
+            {
+                {0, 1, 0, 0, 1},
+                {1, 0, 1, 1, 0},
+                {0, 1, 0, 0, 1},
+                {0, 1, 0, 0, 1},
+                {1, 0, 1, 1, 0}
+            };
+
+            //double[,] arr = new double[,]
+            //{
+            //    {0, 1, 1, 0},
+            //    {0, 0, 1, 1},
+            //    {1, 0, 0, 1},
+            //    {0, 1, 0, 0}
+            //};
+
+
+            Graph g = new Graph(arr);
+
+            g.DFS_Non_recursive(0);
+        }
+
+        class Graph
+        {
+            private double[,] matrix;
+            private int n;
+            public Graph(int n)
+            {
+                matrix = new double[n, n];
+                this.n = n;
+            }
+
+            public Graph(double[,] arr)
+            {
+                matrix = arr;
+                this.n = arr.GetLength(0);
+            }
+
+            public void BFS(int from)
+            {
+                bool[] visited = new bool[n];
+                Queue<int> turn = new Queue<int>();
+                turn.Enqueue(from);
+                visited[from] = true;
+                while (turn.Count != 0)
+                {
+                    int index = turn.Dequeue();
+                    Debug.WriteLine(index);
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (matrix[index, i] != 0 && !visited[i])
+                        {
+                            visited[i] = true;
+                            turn.Enqueue(i);
+                        }
+                    }
+                }
+            }
+
+            public void DFS(int from)
+            {
+                DFS(from, new bool[n]);
+            }
+
+            private void DFS(int from, bool[] visited)
+            {
+                // bool[] visited = new bool[n];
+                visited[from] = true;
+
+                Debug.WriteLine(from);
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (matrix[from, i] != 0 && !visited[i])
+                        DFS(i, visited);
+                }
+            }
+
+            public void DFS_Non_recursive(int from)
+            {
+                bool[] visited = new bool[n];
+                Stack<int> stack = new Stack<int>();
+
+                stack.Push(from);
+                visited[from] = true;
+                while (stack.Count != 0)
+                {
+                    int index = stack.Pop();
+                    Debug.WriteLine(index);
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (matrix[index, i] != 0 && !visited[i])
+                        {
+                            stack.Push(i);
+                            visited[i] = true;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
