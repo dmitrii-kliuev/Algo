e79b467b697fa2f9873820b6708e6b8b95cce1c5
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-09-18 17:57:30 +0300

    + Is substring + Are all characters in the string found only once

---
+++ leetcode.Tests/Algo/IsSubstringTest.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace leetcode.Tests.Algo
+{
+    public class IsSubstringTest
+    {
+        [Theory]
+        [InlineData("hello", "ell", true)]
+        [InlineData("hello", "eql", false)]
+        [InlineData("hello", "o", true)]
+        [InlineData("hello", "h", true)]
+        public void Test(string str, string subStr, bool expected)
+        {
+            var actual = Solution.IsSubstring(str, subStr);
+            Assert.Equal(expected, actual);
+        }
+
+        static class Solution
+        {
+            public static bool IsSubstring(string str, string subStr)
+            {
+                var tmp = "";
+
+                foreach (var s in str)
+                {
+                    tmp += s;
+                    if (tmp.Length == subStr.Length)
+                    {
+                        if (tmp == subStr)
+                            return true;
+
+                        tmp = RemoveFirstLetter(tmp);
+                    }
+                }
+
+                return false;
+            }
+
+            private static string RemoveFirstLetter(string tmp)
+            {
+                var t = "";
+                for (int i = 1; i < tmp.Length; i++)
+                    t += tmp[i];
+
+                return t;
+            }
+        }
+    }
+}
---
+++ leetcode.Tests/Algo/OneTimeCharsInStr.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace leetcode.Tests.Algo
+{
+    public class OneTimeCharsInStr
+    {
+        [Theory]
+        [InlineData("qweasd", true)]
+        [InlineData("qwdeasd", false)]
+        [InlineData("aa", false)]
+        [InlineData("a", true)]
+        public void Test(string str, bool expected)
+        {
+            var actual = Solution.IsOnlyOnce(str);
+            Assert.Equal(expected, actual);
+        }
+
+        static class Solution
+        {
+            public static bool IsOnlyOnce(string str)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (i != j && str[i] == str[j])
+                            return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+    }
+}
