f17c74cd008cd87ec4e6e067252aaff699064841
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-11-27 10:45:55 +0300

    tmp

--- leetcode.Tests/LinkedListPartition.cs
+++ leetcode.Tests/LinkedListPartition.cs
@@ -14,6 +14,19 @@ namespace leetcode.Tests
             EXAMPLE
             Input:  3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5)
             Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+
+            Clarification
+            Rephrased question to make it clearer that x doesn't need to be between the partitions: 
+            "Partition: Write code to partition a linked list around a value x, such that all nodes 
+            less than x come before all nodes greater than or equal to x. (IMPORTANT: The partition 
+            element x can appear anywhere in the “right partition”; it does not need to appear 
+            between the left and right partitions. The additional spacing the example below indicates 
+            the partition.)
+
+            EXAMPLE
+            Input: 	3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+            Output: 	3 -> 1 -> 2       ->        10 -> 5 -> 5 -> 8
         */
 
         [Theory]
