bbac86a2252195ac2681f5ad28be6aaa4be086f8
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2019-12-04 14:34:59 +0300

    + task description

--- leetcode.Tests/SumLists.cs
+++ leetcode.Tests/SumLists.cs
@@ -8,6 +8,20 @@ namespace leetcode.Tests
 {
     public class SumLists
     {
+        /*
+            Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+            digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a
+            function that adds the two numbers and returns the sum as a linked list.
+            EXAMPLE
+            Input: (7-> 1 -> 6) + (5 -> 9 -> 2) . Thatis,617 + 295.
+            Output: 2 -> 1 -> 9. That is, 912 .
+            FOLLOW UP
+            Suppose the digits are stored in forward order. Repeat the above problem.
+            EXAMPLE
+            Input: (6 -> 1 -> 7) + (2 -> 9 ->) 5) . Thatis,617 + 295.
+            Output: 9 -> 1 -> 2. That is, 912
+        */
+
         [Theory]
         [InlineData(new[] { 7, 1, 6 }, new[] { 5, 9, 2 }, new[] { 2, 1, 9 })]
         [InlineData(new[] { 7, 1, 6 }, new[] { 5, 9 }, new[] { 2, 1, 7 })]
