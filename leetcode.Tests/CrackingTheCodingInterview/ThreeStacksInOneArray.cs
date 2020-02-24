c23c0195d0da1bc5fa74bc2bd9a96f1c029438fc
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2020-02-24 11:49:46 +0300

    +  three stacks implementation in single array. Dynamic size solution.

--- leetcode.Tests/CrackingTheCodingInterview/ThreeStacksInOneArray.cs
+++ leetcode.Tests/CrackingTheCodingInterview/ThreeStacksInOneArray.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
 using System.Text;
 using Xunit;
 
@@ -9,9 +11,9 @@ namespace leetcode.Tests.CrackingTheCodingInterview
     {
         /*3.1 Three in One: Describe how you could use a single array to implement three stacks.*/
 
-        [Theory]
-        [InlineData("", true)]
-        public void Test(string str, bool expected)
+        #region fixed size
+        [Fact]
+        public void FixedSizeTest()
         {
             var s = new Solution(capacity: 4, stackQty: 3);
 
@@ -114,6 +116,201 @@ namespace leetcode.Tests.CrackingTheCodingInterview
                 return stackNum * capacity + sizes[stackNum];
             }
         }
+        #endregion
+
+        #region flexible size
+        [Fact]
+        public void FlexibleSizeTest()
+        {
+            var s = new MultiStack(numberOfStacks: 3, defaultCapacity: 4);
+
+            s.Push(0, 55);
+            s.Push(0, 54);
+            s.Push(0, 57);
+
+            s.Push(1, 24);
+            s.Push(1, 80);
+
+            s.Push(2, 17);
+            s.Push(2, 27);
+            s.Push(2, 37);
+            s.Push(2, 47);
+
+            //Assert.True(s.IsFull(2));
+
+            //Assert.Throws<FullStackException>(() => s.Push(2, 100));
+            s.Push(2, 100);
+
+            Assert.Equal(57, s.Pop(0));
+
+            Assert.Equal(54, s.Peek(0));
+            Assert.Equal(54, s.Pop(0));
+
+            Assert.Equal(80, s.Peek(1));
+            Assert.Equal(80, s.Pop(1));
+
+            Assert.Equal(47, s.Pop(2));
+            Assert.Equal(37, s.Pop(2));
+            Assert.Equal(27, s.Pop(2));
+            Assert.Equal(17, s.Pop(2));
+
+            Assert.Throws<EmptyStackException>(() => s.Pop(2));
+
+            //Assert.True(s.IsEmpty(2));
+        }
+
+        private class MultiStack
+        {
+            private class StackInfo
+            {
+                public int start, size, capacity;
+                public StackInfo(int start, int capacity)
+                {
+                    this.start = start;
+                    this.capacity = capacity;
+                }
+
+                public bool IsWithinStackCapacity(int index, int[] values)
+                {
+                    if (index < 0 || index > values.Length)
+                        return false;
+
+                    int contiguousIndex = index < start ? index + values.Length : index;
+                    int end = start + capacity;
+                    return start <= contiguousIndex && contiguousIndex < end;
+                }
+
+                public int LastCapacityIndex()
+                {
+                    return AdjustIndex(start + capacity - 1);
+                }
+
+                public int LastElementIndex()
+                {
+                    return AdjustIndex(start + size - 1);
+                }
+
+                public bool IsFull()
+                {
+                    return size == capacity;
+                }
+
+                public bool IsEmpty()
+                {
+                    return size == 0;
+                }
+            }
+
+            private readonly StackInfo[] info;
+            public static int[] values;
+
+            public MultiStack(int numberOfStacks, int defaultCapacity)
+            {
+                info = new StackInfo[numberOfStacks];
+
+                for (int i = 0; i < info.Length; i++)
+                    info[i] = new StackInfo(defaultCapacity * i, defaultCapacity);
+
+                values = new int[numberOfStacks * defaultCapacity];
+            }
+
+            public void Push(int stackNum, int value)
+            {
+                if (AllStacksAreFull()) throw new FullStackException();
+
+                var stack = info[stackNum];
+
+                if (stack.IsFull())
+                    Expand(stackNum);
+
+                stack.size++;
+
+                values[stack.LastElementIndex()] = value;
+            }
+
+            public int Pop(int stackNum)
+            {
+                var stack = info[stackNum];
+
+                if (stack.IsEmpty()) throw new EmptyStackException();
+
+                var value = values[stack.LastElementIndex()];
+                values[stack.LastElementIndex()] = 0;
+                stack.size--;
+
+                return value;
+            }
+
+            public int Peek(int stackNum)
+            {
+                var stack = info[stackNum];
+                if (stack.IsEmpty()) throw new EmptyStackException();
+
+                return values[stack.LastElementIndex()];
+            }
+
+            private void Shift(int StackNum)
+            {
+                var stack = info[StackNum];
+
+                if (stack.size >= stack.capacity)
+                {
+                    int nextStack = (StackNum + 1) % info.Length;
+                    Shift(nextStack);
+                    stack.capacity++;
+                }
+
+                int index = stack.LastCapacityIndex();
+                while (stack.IsWithinStackCapacity(index, values))
+                {
+                    values[index] = values[PreviousIndex(index)];
+                    index = PreviousIndex(index);
+                }
+
+                values[stack.start] = 0;
+                stack.start = NextIndex(stack.start);
+                stack.capacity--;
+            }
+
+            private void Expand(int stackNum)
+            {
+                Shift((stackNum + 1) % info.Length);
+                info[stackNum].capacity++;
+            }
+
+            private int NumberOfElements()
+            {
+                int size = 0;
+                foreach (var item in info)
+                    size += item.size;
+
+                return size;
+            }
+
+            private bool AllStacksAreFull()
+            {
+                return NumberOfElements() == values.Length;
+            }
+
+            public static int AdjustIndex(int index)
+            {
+                int max = values.Length;
+                return ((index % max) + max) % max;
+            }
+
+            private int NextIndex(int index)
+            {
+                return AdjustIndex(index + 1);
+            }
+
+            public static int PreviousIndex(int index)
+            {
+                return AdjustIndex(index - 1);
+            }
+
+
+        }
+        #endregion
     }
 
     public class EmptyStackException : Exception
