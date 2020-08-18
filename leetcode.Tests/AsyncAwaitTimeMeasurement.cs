eabe5e842fc08a01144907c2943d2697ce3a469e
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2020-08-18 22:59:01 +0300

    +AsyncAwaitTimeMeasurement

---
+++ leetcode.Tests/AsyncAwaitTimeMeasurement.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Algo.Tests
+{
+    public class AsyncAwaitTimeMeasurement
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public AsyncAwaitTimeMeasurement(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler", Justification = "<Pending>")]
+        public async Task Test()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var res = await Method1().ContinueWith(x =>
+            {
+                sw.Stop();
+                _testOutputHelper.WriteLine($"Elapsed milliseconds: {sw.ElapsedMilliseconds}");
+
+                return x.Result;
+            }).ConfigureAwait(false);
+
+            _testOutputHelper.WriteLine($"res: {res}");
+        }
+
+        public async Task<int> Method1()
+        {
+            await Task.Delay(2000).ConfigureAwait(false);
+            return 100500;
+        }
+    }
+}
