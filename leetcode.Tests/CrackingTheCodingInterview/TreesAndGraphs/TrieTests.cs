58b4ba280948dbd7ca31c7bd561ec81b1f538cd8
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2021-06-15 21:05:33 +0300

    + trie getWords, + search suggestions

--- leetcode.Tests/CrackingTheCodingInterview/TreesAndGraphs/TrieTests.cs
+++ leetcode.Tests/CrackingTheCodingInterview/TreesAndGraphs/TrieTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace Algo.Tests.CrackingTheCodingInterview.TreesAndGraphs
@@ -38,51 +39,42 @@ namespace Algo.Tests.CrackingTheCodingInterview.TreesAndGraphs
             if (trie.Search("thaw"))
                 Debug.WriteLine("thaw --- " + present);
             else Debug.WriteLine("thaw --- " + absent);
+
+            var words = trie.GetWords();
+            foreach (var word in words)
+            {
+                Debug.WriteLine(word);
+            }
+
+            Debug.WriteLine("-------th-------");
+            foreach (var word in trie.SearchSuggestions("th", 20))
+            {
+                Debug.WriteLine(word);
+            }
+
+            Debug.WriteLine("-------a-------");
+            foreach (var word in trie.SearchSuggestions("a", 20))
+            {
+                Debug.WriteLine(word);
+            }
+
+            Debug.WriteLine("-------b-------");
+            foreach (var word in trie.SearchSuggestions("b", 20))
+            {
+                Debug.WriteLine(word);
+            }
+
         }
     }
 
     public class Trie
     {
-        const int ALPHABET_SIZE = 26;
+        private const int ALPHABET_SIZE = 26;
         private readonly TrieNode _root;
 
         public Trie()
         {
             _root = new TrieNode();
-        }
-
-        public void Insert(string key)
-        {
-            var pCrawl = _root;
-
-            for (var level = 0; level < key.Length; level++)
-            {
-                var index = key[level] - 'a';
-                pCrawl.Children[index] ??= new TrieNode();
-
-                pCrawl = pCrawl.Children[index];
-            }
-
-            pCrawl.IsEndOfWord = true;
-        }
-
-        public bool Search(string key)
-        {
-            var pCrawl = _root;
-
-            for (var level = 0; level < key.Length; level++)
-            {
-                var index = key[level] - 'a';
-
-                if (pCrawl.Children[index] == null)
-                {
-                    return false;
-                }
-
-                pCrawl = pCrawl.Children[index];
-            }
-
-            return pCrawl != null && pCrawl.IsEndOfWord;
         }
 
         private class TrieNode
@@ -99,6 +91,90 @@ namespace Algo.Tests.CrackingTheCodingInterview.TreesAndGraphs
                     Children[i] = null;
                 }
             }
+        }
+
+        public void Insert(string key)
+        {
+            var node = _root;
+
+            for (var level = 0; level < key.Length; level++)
+            {
+                var index = key[level] - 'a';
+                node.Children[index] ??= new TrieNode();
+
+                node = node.Children[index];
+            }
+
+            node.IsEndOfWord = true;
+        }
+
+        public bool Search(string key)
+        {
+            var node = _root;
+
+            for (var level = 0; level < key.Length; level++)
+            {
+                var index = key[level] - 'a';
+
+                if (node.Children[index] == null)
+                {
+                    return false;
+                }
+
+                node = node.Children[index];
+            }
+
+            return node != null && node.IsEndOfWord;
+        }
+
+        public IEnumerable<string> GetWords(int quantity = 0)
+        {
+            const string str = "";
+            var words = new List<string>();
+            GetWords(_root, str, words, quantity);
+
+            return words;
+        }
+
+        void GetWords(TrieNode node, string str, ICollection<string> words, int quantity = 0)
+        {
+            if (quantity != 0 && words.Count == quantity) return;
+
+            if (node.IsEndOfWord)
+            {
+                words.Add(str);
+            }
+
+            for (var i = 0; i < ALPHABET_SIZE; i++)
+            {
+                if (node.Children[i] != null)
+                {
+                    var newStr = str + (char)(i + 'a');
+                    GetWords(node.Children[i], newStr, words, quantity);
+                }
+            }
+        }
+
+        public IEnumerable<string> SearchSuggestions(string start, int quantity = 0)
+        {
+            var suggestions = new List<string>();
+            if (start == null) return suggestions;
+
+            var node = _root;
+
+            for (var level = 0; level < start.Length; level++)
+            {
+                var index = start[level] - 'a';
+
+                if (node.Children[index] != null)
+                {
+                    node = node.Children[index];
+                }
+            }
+
+            GetWords(node, start, suggestions, quantity);
+
+            return suggestions;
         }
     }
 }
