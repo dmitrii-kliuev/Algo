7e493fc4aec6f44e42dd6fac0ed0722343fe8fab
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2024-06-11 13:42:17 +0300

    1920. Build Array from Permutation

---
+++ leetcode.Tests/leetcode/BuildArrayFromPermutation.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algo.Tests.leetcode
+{
+    public class BuildArrayFromPermutation
+    {
+        [Theory]
+        [InlineData(new int[] { 0, 2, 1, 5, 3, 4 }, new int[] { 0, 1, 2, 4, 5, 3 })]
+        [InlineData(new int[] { 5, 0, 1, 2, 3, 4 }, new int[] { 4, 5, 0, 1, 2, 3 })]
+        public void Test(int[] nums, int[] expected)
+        {
+            var result = BuildArrayWithEuclidsDivisionAlgorithm(nums);
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        public int[] BuildArray(int[] nums)
+        {
+            var ans = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+
+        public int[] BuildArrayWithEuclidsDivisionAlgorithm(int[] nums)
+        {
+            int n = nums.Length;
+
+            var q = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                var r = nums[i];
+
+            /*
+                retrieve correct value from potentially already processed element
+                i.e. get r from something maybe already in the form a = qb + r
+                if it isnt already processed (doesnt have qb yet), that's ok b/c
+                r < q, so r % q will return the same value
+            */
+            var b = nums[nums[i]] % q;
+
+            //put it all together
+            nums[i] = q * b + r;
+
+                // https://www.cuemath.com/numbers/euclids-division-algorithm/
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var a = nums[i];
+                nums[i] = a / q; //  // i = 0, nums[i] = 29, n = 6. nums[0] = 29 / 6 = 4
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ leetcode.Tests/leetcode/ConcatenationOfArray.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xunit;
+
+namespace Algo.Tests.leetcode
+{
+/*
+    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+    Specifically, ans is the concatenation of two nums arrays.
+
+    Return the array ans.
+
+ 
+
+    Example 1:
+
+    Input: nums = [1,2,1]
+    Output: [1,2,1,1,2,1]
+    Explanation: The array ans is formed as follows:
+    - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+    - ans = [1,2,1,1,2,1]
+    Example 2:
+
+    Input: nums = [1,3,2,1]
+    Output: [1,3,2,1,1,3,2,1]
+    Explanation: The array ans is formed as follows:
+    - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+    - ans = [1,3,2,1,1,3,2,1]
+ 
+
+    Constraints:
+
+    n == nums.length
+    1 <= n <= 1000
+    1 <= nums[i] <= 1000
+
+*/
+
+    public class ConcatenationOfArray
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 1 }, new int[] { 1, 2, 1, 1, 2, 1 })]
+        [InlineData(new int[] { 1, 3, 2, 1 }, new int[] { 1, 3, 2, 1, 1, 3, 2, 1 })]
+        public void Test(int[] nums, int[] expected)
+        {
+            var res = GetConcatenation(nums);
+
+            res.Should().BeEquivalentTo(expected);
+        }
+
+        public int[] GetConcatenation(int[] nums)
+        {
+            var len = nums.Length;
+            var ans = new int[len * 2];
+
+            for (int i = 0; i < len; i++)
+            {
+                ans[i] = nums[i];
+                ans[i + len] = nums[i];
+            }
+
+            return ans;
+        }
+    }
+}
