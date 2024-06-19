e20deeef01c640f71570869f4601867734f0c0ca
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2024-06-19 14:42:12 +0300

    wip

---
+++ leetcode.Tests/leetcode/SmallestEvenMultiple.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Algo.Tests.leetcode
+{
+    public class SmallestEvenMultipleSolution
+    {
+        [Theory]
+        [InlineData(5, 10)]
+        [InlineData(6, 6)]
+        public void Test(int n, int expected)
+        {
+            var actual = SmallestEvenMultiple(n);
+            Assert.Equal(expected, actual);
+        }
+
+        public int SmallestEvenMultiple(int n)
+        {
+            if (n % 2 == 0)
+                return n;
+
+            return 2 * n;
+        }
+    }
+}
