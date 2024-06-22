410f20c56519c6eaf5856765dda971ce9d1b1eb8
dmitrii-kliuev-1989 <dmitrii.kliuev.1989@gmail.com>
2024-06-22 16:08:09 +0300

    wip

---
+++ leetcode.Tests/leetcode/CountPairsWhoseSumIsLessThanTarget.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Algo.Tests.leetcode
+{
+    public class CountPairsWhoseSumIsLessThanTarget
+    {
+        [Theory]
+        [InlineData(new int[] { -1, 1, 2, 3, 1 }, 2, 3)]
+        [InlineData(new int[] { -6, 2, 5, -2, -7, -1, 3 }, -2, 10)]
+        public void Test(IList<int> nums, int target, int expected)
+        {
+            var actual = CountPairs(nums, target);
+            Assert.Equal(expected, actual);
+        }
+
+        public int CountPairs(IList<int> nums, int target)
+        {
+            var count = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    var sum = nums[i] + nums[j];
+                    if (sum < target)
+                        count++;
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
---
+++ leetcode.Tests/leetcode/FindTheNumberOfGoodPairsI.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Algo.Tests.leetcode
+{
+    public class FindTheNumberOfGoodPairsI
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3, 4 }, new int[] { 1, 3, 4 }, 1, 5)]
+        [InlineData(new int[] { 1, 2, 4, 12 }, new int[] { 2, 4 }, 3, 2)]
+        public void Test(int[] nums1, int[] nums2, int k, int expected)
+        {
+            var actual = NumberOfPairs(nums1, nums2, k);
+            Assert.Equal(expected, actual);
+        }
+
+        public int NumberOfPairs(int[] nums1, int[] nums2, int k)
+        {
+            var res = 0;
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] % (nums2[j] * k) == 0)
+                    {
+                        res++;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
